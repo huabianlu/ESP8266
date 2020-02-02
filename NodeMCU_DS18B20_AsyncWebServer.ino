64b66f706919a1cea648d271939d6dee90d06b7a
huabianlu <58382925+huabianlu@users.noreply.github.com>
2020-02-02 16:39:19 +0800

    在Web服务器上异步显示DS18B20传感器读数

---
+++ NodeMCU_DS18B20_AsyncWebServer.ino
@@ -0,0 +1,175 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com  
+*********/
+
+// Import required libraries导入所需的库
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <ESPAsyncWebServer.h>
+#else
+  #include <Arduino.h>
+  #include <ESP8266WiFi.h>
+  #include <Hash.h>
+  #include <ESPAsyncTCP.h>
+  #include <ESPAsyncWebServer.h>  //异步服务器
+#endif
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is connected to GPIO 4
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// Replace with your network credentials
+const char* ssid = "WL-911";
+const char* password = "123456789";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readDSTemperatureC() {  //所述readDSTemperatureC（）函数返回以摄氏度读数
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  float tempC = sensors.getTempCByIndex(0);
+
+  if(tempC == -127.00) {
+    Serial.println("Failed to read from DS18B20 sensor");
+    return "--";
+  } else {
+    Serial.print("Temperature Celsius: ");
+    Serial.println(tempC); 
+  }
+  return String(tempC);  //读数以字符串类型返回。要将float转换为字符串，请使用String（）函数。
+}
+
+String readDSTemperatureF() {
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  float tempF = sensors.getTempFByIndex(0);
+
+  if(int(tempF) == -196){
+    Serial.println("Failed to read from DS18B20 sensor");
+    return "--";
+  } else {
+    Serial.print("Temperature Fahrenheit: ");
+    Serial.println(tempF);
+  }
+  return String(tempF);
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .ds-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP DS18B20 Server</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="ds-labels">Temperature Celsius</span> 
+    <span id="temperaturec">%TEMPERATUREC%</span>  
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="ds-labels">Temperature Fahrenheit</span>
+    <span id="temperaturef">%TEMPERATUREF%</span> 
+    <sup class="units">&deg;F</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperaturec").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperaturec", true);
+  xhttp.send();
+}, 10000) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperaturef").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperaturef", true);
+  xhttp.send();
+}, 10000) ;
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with DHT values
+String processor(const String& var){   //创建processor（）函数，该函数将用实际温度值替换HTML文本中的占位符。
+  //Serial.println(var);
+  if(var == "TEMPERATUREC"){
+    return readDSTemperatureC();
+  }
+  else if(var == "TEMPERATUREF"){  //如果占位符为％TEMPERATUREF％，则以华氏温度返回温度。
+    return readDSTemperatureF();
+  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  Serial.println();
+  
+  // Start up the DS18B20 library
+  sensors.begin();
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);  //当我们对根URL发出请求时，我们将发送存储在index_html变量中的HTML文本。我们还需要传递处理器函数，该函数将用正确的值替换所有占位符。
+  });
+  server.on("/temperaturec", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDSTemperatureC().c_str());  //我们需要添加两个其他处理程序来更新温度读数。当我们在/ temperature c URL 上收到请求时  ，我们只需要发送更新的温度值。它是纯文本，应该以char形式发送，因此我们使用c_str（）方法。
+  });
+  server.on("/temperaturef", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDSTemperatureF().c_str());
+  });
+  // Start server
+  server.begin();
+}
+ 
+void loop(){    //因为这是一个异步Web服务器，所以我们不需要在loop（）中编写任何内容。
+  
+}
